146991c8cbdda5254147598fe0e2a81e8f8343d4
ekkennyko <vip.dvorakovskiy@gmail.com>
2019-02-20 15:37:52 +0900

    Test project

---
+++ Proj1/Proj1/main.cpp
@@ -0,0 +1,14 @@
+#ifndef TEST
+#include <iostream>
+#include "numbers.h"
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	cout << "Hello there" <<endl;
+	system("pause");
+	return 0;
+}
+
+#endif
