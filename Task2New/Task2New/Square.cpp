4ded2d250c9d1e78a17244893059769ee048cb6f
vmpfiit <wansle@mail.ru>
2019-06-05 15:10:13 +0900

    Task2 up

--- Task2New/Task2New/Square.cpp
+++ Task2New/Task2New/Square.cpp
@@ -1,33 +1,40 @@
 //#include <vector>
-//#include <random>
 //#include <iostream>
-//
+//#include <ctime>
+//#include <random> 
 //using namespace std;
 //
 //struct Point
 //{
-//	double x, y;
+//	double x,y;
 //};
 //
-//double generateRandom(double Min, double Max){
-//	random_device rd;
-//	mt19937 gen(rd());
-//	uniform_real_distribution<> uid(Min, Max);
+//double fRandom(double fMin, double fMax)
+//{
+//	random_device rd; 
+//    mt19937 gen(rd());  
+//    uniform_real_distribution<> uid(fMin, fMax); 
 //	return uid(gen);
 //}
 //
-//void main(){
+//void main()
+//{
 //	setlocale(0, "");
-//	int countPoint = 100;
-//	double lengthSquare = 100;
-//	Point *points = new Point[countPoint];
-//	for(int i = 0; i < countPoint; i++){
-//		points[i].x = generateRandom(0, lengthSquare);
-//		points[i].y = generateRandom(0, lengthSquare);
+//	int n = 100;
+//	double a = 100;
+//	cout<<"Введите кол-во точек и длину стороны квадрата: ";
+//	cin>>n>>a;
+//	Point *points = new Point[n];
+//	for (int i=0; i<n; i++)
+//	{
+//		points[i].x = fRandom(0, a);
+//		points[i].y = fRandom(0, a);
 //	}
-//	cout<<"Точки внутри квадрата"<<endl;
-//	for(int i = 0; i < countPoint; i++){
-//		cout<<i+1<<": x - "<<points[i].x<<" | y - "<<points[i].y<<endl;
+//	cout<<"Точки внутри квадрата со сторонами "<<a<<" на "<<a<<":"<<endl;
+//	for (int i=0; i<n; i++)
+//	{
+//		cout.precision(0);
+//		cout<<fixed<<i+1<<") x: "<<points[i].x<< " y: "<<points[i].y<<endl;
 //	}
 //	system("pause");
 //}
