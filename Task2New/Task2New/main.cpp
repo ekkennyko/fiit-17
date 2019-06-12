a38653bc67182c1e21d5e85efdc33f26487eb692
ekkenny <dhrhrhjdjfh@gmail.com>
2019-06-13 08:51:54 +0900

    Update Task 2

---
+++ Task2New/Task2New/main.cpp
@@ -0,0 +1,15 @@
+#ifndef TEST
+
+#include <iostream>
+#include "subj.h"
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	cout << "Hello there" << endl;
+	system("pause");
+	return 0;
+}
+
+#endif
